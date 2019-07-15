cca35bbef912c841b4c6186854c74bed758adca5
curtis cherry <trecherry55@gmail.com>
2019-07-14 21:28:23 -0400

    curtis made a commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻🇪🇻</h1>
+    <h1></h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>curtis made a commit</div>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
