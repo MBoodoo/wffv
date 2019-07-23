edd732a608fb20dce9a90a1ede063292cae9f884
Mike Boodoo <chrisboo99@gmail.com>
2019-07-23 07:36:33 -0400

    Fix 1.71

--- src/pages/Screenings.js
+++ src/pages/Screenings.js
@@ -9,9 +9,7 @@ export default () => (
   <Layout>
     <SEO title="Screenings" />
     <h1>Screenings</h1>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    <div>Lorem ipsum dolor amet sustainable deep v flannel lyft banjo, coloring book shoreditch asymmetrical distillery pork belly gentrify. Etsy edison bulb kickstarter mumblecore roof party. Vaporware freegan cardigan hammock distillery. Tumblr venmo fingerstache four loko, selfies </div>
-    </div>
+    
   </Layout>
 )
 
